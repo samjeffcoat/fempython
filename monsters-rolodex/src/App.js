041eb417b59e55fe31722ea67acd820709feae26
Sam <Jeffcoat>
2019-11-01 16:58:26 -0500

    Moves search bar to a functional component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import './App.css';
-import {CardList} from './components/card-list/card-list.component'
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBar } from './components/search-bar/search-box.component';
+
 
 class App extends Component{
     constructor(){
@@ -22,9 +24,12 @@ class App extends Component{
       )
     return (
     <div className= 'App'>
-        <input type ='search' 
-        placeholder = 'search monsters' 
-        onChange = {e => this.setState({searchField: e.target.value}, () => console.log(this.state) )} />
+        
+        <SearchBar
+            placeholder= 'search monsters'
+                handleChange={ e => this.setState({searchField: e.target.value})}
+        
+        />
         <CardList monsters= {filteredMonsters}>
        
     </CardList>
