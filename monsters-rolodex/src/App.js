cad872517233c1d2d98b11f74a691fb175cfa2b1
Sam <Jeffcoat>
2019-11-01 16:07:20 -0500

    Ability to filter monsters

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,7 +6,8 @@ class App extends Component{
     constructor(){
         super();
         this.state= {
-            monsters: []
+            monsters: [],
+            searchField: ' '
         }
     }
     componentDidMount(){
@@ -15,9 +16,16 @@ class App extends Component{
         .then(users => this.setState({monsters: users}))
     }
   render(){
+      const { monsters, searchField  } = this.state;
+      const filteredMonsters = monsters.filter(monster => 
+          monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
     return (
     <div className= 'App'>
-        <CardList monsters= {this.state.monsters}>
+        <input type ='search' 
+        placeholder = 'search monsters' 
+        onChange = {e => this.setState({searchField: e.target.value}, () => console.log(this.state) )} />
+        <CardList monsters= {filteredMonsters}>
        
     </CardList>
     </div >
