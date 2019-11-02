16c05775d1545e98d7d93478737713a4b7475d67
Sam <Jeffcoat>
2019-11-02 14:00:16 -0500

    Updates

---
+++ precache-manifest.62fd1b4c1f13bd127f6e77b41adf5d4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd4a73633598603dd6198e9d126f83ef",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ddd78bf744fe306b6ad0",
+    "url": "/monsters-rolodex/static/css/main.0ab32a9e.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "ddd78bf744fe306b6ad0",
+    "url": "/monsters-rolodex/static/js/main.1ecb834f.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
